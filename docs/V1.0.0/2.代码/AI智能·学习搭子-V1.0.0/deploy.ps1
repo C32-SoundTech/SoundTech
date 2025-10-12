207bf0b9597ec255d01b21826371d130fd4c3796
yuz <2778582993@qq.com>
2025-10-12 23:43:30 +0800

    一键部署包

--- docs/V1.0.0/2.代码/AI智能·学习搭子-V1.0.0/deploy.ps1
+++ docs/V1.0.0/2.代码/AI智能·学习搭子-V1.0.0/deploy.ps1
@@ -1,5 +1,6 @@
-Write-Host "正在下载Python 3.14.0 64位嵌入版本..."
-curl https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip --output .\python314-embed.zip
+Clear-Host
+Write-Host "Downloading Python 3.14.0 64-bit embedded version..."
+curl https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -o .\python314-embed.zip
 Clear-Host
 
 New-Item -Path .\python314-embed -ItemType Directory
@@ -8,17 +9,18 @@ Remove-Item -Path .\python314-embed.zip
 Clear-Host
 
 Set-Location .\python314-embed
-Set-Content -Path ".\python314._pth" -Value @"
+Set-Content -Path ".\python314._pth" -Encoding ascii -Value @"
 python314.zip
 .
 ..
 import site
 "@
 Clear-Host
-Write-Host "正在下载项目部署依赖..."
-curl https://bootstrap.pypa.io/get-pip.py --output .\get-pip.py 
+Write-Host "Downloading necessary requirements..."
+curl https://bootstrap.pypa.io/get-pip.py -o .\get-pip.py 
 .\python.exe .\get-pip.py --no-warn-script-location
 .\python.exe -m pip install -r ..\requirements.txt --index-url https://mirrors.aliyun.com/pypi/simple/ --no-warn-script-location
+Set-Location ..
 Clear-Host
 
-Set-Location ..
+.\python314-embed\python.exe .\app.py
