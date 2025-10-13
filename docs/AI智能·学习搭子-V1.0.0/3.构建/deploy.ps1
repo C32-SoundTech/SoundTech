9760c8b2098d72114d6b43c2801cd37c9b447464
丁炜宸 <2312190131@pop.zjgsu.edu.cn>
2025-10-13 15:02:58 +0800

    refactor: 重构部署脚本和文档结构
    优化部署脚本逻辑，添加详细注释说明
    移除冗余文件，整理文档目录结构
    更新.gitignore排除zip文件
    调整requirements.txt文件位置和内容

--- docs/AI智能·学习搭子-V1.0.0/3.构建/deploy.ps1
+++ docs/AI智能·学习搭子-V1.0.0/3.构建/deploy.ps1
@@ -1,13 +1,15 @@
+# 构建工具程序（官方镜像源）
+# 适用于已经下载项目源码，请在项目源码对应的文件夹下运行
+# 若未下载项目源码，可运行【4.部署】文件夹下的一键部署脚本
+
 Clear-Host
 Write-Host "Downloading Python 3.14.0 64-bit embedded version..."
 curl https://www.python.org/ftp/python/3.14.0/python-3.14.0-embed-amd64.zip -o .\python314-embed.zip
-Clear-Host
 
+Clear-Host
 New-Item -Path .\python314-embed -ItemType Directory
 Expand-Archive -Path .\python314-embed.zip -DestinationPath .\python314-embed
 Remove-Item -Path .\python314-embed.zip
-Clear-Host
-
 Set-Location .\python314-embed
 Set-Content -Path ".\python314._pth" -Encoding ascii -Value @"
 python314.zip
@@ -15,12 +17,13 @@ python314.zip
 ..
 import site
 "@
+
 Clear-Host
 Write-Host "Downloading necessary requirements..."
 curl https://bootstrap.pypa.io/get-pip.py -o .\get-pip.py 
 .\python.exe .\get-pip.py --no-warn-script-location
 .\python.exe -m pip install -r ..\requirements.txt --index-url https://mirrors.aliyun.com/pypi/simple/ --no-warn-script-location
 Set-Location ..
-Clear-Host
 
+Clear-Host
 .\python314-embed\python.exe .\app.py
