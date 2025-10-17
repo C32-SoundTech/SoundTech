80c8d7952d516f0029e041f86611605d3910946d
丁炜宸 <2312190131@pop.zjgsu.edu.cn>
2025-10-17 23:36:48 +0800

    feat: 添加毛泽东思想与中国特色社会主义理论体系概论相关资源及配置
    新增毛泽东思想与中国特色社会主义理论体系概论PDF资源文件
    添加相关配置文件包括log_config.toml、custom_key.toml和config.toml
    实现知识库相关组件包括maoist_knowledge.py及对应的yaml配置
    添加测试脚本db_init.py和maoist_rag_agent.py用于知识库初始化和问答测试
    更新.gitignore和requirements.txt以适配新功能

---
+++ rag_agent/test/db_init.py
@@ -0,0 +1,19 @@
+import warnings
+warnings.filterwarnings("ignore", module="jieba")
+warnings.filterwarnings("ignore", module="pydantic")
+
+import jieba
+jieba.setLogLevel(jieba.logging.INFO)
+
+from agentuniverse.base.agentuniverse import AgentUniverse
+from agentuniverse.agent.action.knowledge.knowledge_manager import KnowledgeManager
+
+
+if __name__ == '__main__':
+    AgentUniverse().start(config_path='../config/config.toml', core_mode=True)
+    maoist_store_list = ["maoist_chroma_store", "maoist_sqlite_store"]
+    maoist_knowledge = KnowledgeManager().get_instance_obj("maoist_knowledge")
+    maoist_knowledge.insert_knowledge(
+        source_path="../resources/毛泽东思想与中国特色社会主义理论体系概论.pdf",
+        stores=maoist_store_list
+    )
---
+++ rag_agent/test/maoist_rag_agent.py
@@ -0,0 +1,34 @@
+import warnings
+warnings.filterwarnings("ignore", module="jieba")
+warnings.filterwarnings("ignore", module="pydantic")
+
+import jieba
+jieba.setLogLevel(jieba.logging.INFO)
+
+from agentuniverse.agent.output_object import OutputObject
+from agentuniverse.agent.agent import Agent
+from agentuniverse.agent.agent_manager import AgentManager
+from agentuniverse.base.agentuniverse import AgentUniverse
+
+AgentUniverse().start(config_path='../config/config.toml', core_mode=True)
+
+
+def chat(question: str):
+    instance: Agent = AgentManager().get_instance_obj('rag_agent')
+    output_object: OutputObject = instance.run(input=question)
+
+    question = f"\nYour event is :\n"
+    question += output_object.get_data('input')
+    print(question)
+
+    background_info = f"\nRetrieved background is :\n"
+    background_info += output_object.get_data('background').replace("\n","")
+    print(background_info)
+
+    res_info = f"\nRag chat bot execution result is :\n"
+    res_info += output_object.get_data('output')
+    print(res_info)
+
+
+if __name__ == '__main__':
+    chat("科学地评价毛泽东同志一生的贡献")
